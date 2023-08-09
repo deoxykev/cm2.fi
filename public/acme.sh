6fbb16acb92206fcd51d5b90e0c8f41391d5b1c4
Kevin Pham <kevin@deoxy.net>
2023-08-09 13:30:11 -0500

    update acme script for split dns workaround

--- public/acme.sh
+++ public/acme.sh
@@ -23,11 +23,6 @@ TMP=$(mktemp --directory)
 WD="$(pwd)"
 
 die() {
-  if [ -f $TMP/resolv.conf.bak ]; then
-    echo "[+] restoring /etc/resolv.conf"
-    cp $TMP/resolv.conf.bak /etc/resolv.conf
-  fi
-
   if [ -d "$TMP" ]; then
     cd "$WD"
     echo "[+] cleaning up $TMP"
@@ -45,16 +40,11 @@ chmod +x lego
 cp lego /usr/local/bin/
 which lego
 
-# this is a workaround because of split DNS
-echo "[+] modifying resolver temporarily"
-cp /etc/resolv.conf $TMP/resolv.conf.bak
-echo "nameserver 1.1.1.1" > /etc/resolv.conf
-
 cd
 echo "[+] issuing certs"
-/usr/local/bin/lego --accept-tos --email $LETSENCRYPT_EMAIL --dns cloudflare --domains $DOMAINS run
+/usr/local/bin/lego --dns.resolver 1.1.1.1 --accept-tos --email $LETSENCRYPT_EMAIL --dns cloudflare --domains $DOMAINS run
 
-CRONJOB="3 3 * * * /usr/local/bin/lego --accept-tos --email $LETSENCRYPT_EMAIL --dns cloudflare --domains $DOMAINS renew"
+CRONJOB="3 3 * * * /usr/local/bin/lego --dns.resolver 1.1.1.1 --accept-tos --email $LETSENCRYPT_EMAIL --dns cloudflare --domains $DOMAINS renew"
 if crontab -l | grep -q "$DOMAINS renew"; then
   echo "[>] already have a cronjob, skipping crontab install."
 else
